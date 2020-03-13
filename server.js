9b18314bb9126dd8a02ea5a82db5dd0169399883
linjies <1767850644@qq.com>
2020-03-13 22:21:45 +0800

    ..

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+
+app.use("/", express.static(path.join(__dirname, './dist')));
+
+app.get("/", (_, res) => {
+    res.sendFile(__dirname, "./dist", './index.html')
+})
+
+app.listen(8080, () => {
+    console.log(`服务器启动成功 ~~`);
+
+})
